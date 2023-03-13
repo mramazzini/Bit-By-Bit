68ddf1cd5aa930e8c3b227ee9533980a5eeaecc4
mramazzini <mramazzini123@gmail.com>
2023-03-13 08:15:43 -0500

    Added server

---
+++ client/src/components/home/Dashboard.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { useState } from "react";
+import "../../styles/Home.css";
+
+const Dashboard = ({ score }) => {
+  const [activeTab, setActiveTab] = useState("tab1");
+
+  const handleTabClick = (tabName) => {
+    setActiveTab(tabName);
+  };
+  return (
+    <div className="dashboard">
+      <ul className="dashboard-nav">
+        <li
+          className={`dashboard-nav-item ${
+            activeTab === "editor" ? "active" : ""
+          }`}
+          onClick={() => handleTabClick("editor")}
+        >
+          Problems
+        </li>
+        <li
+          className={`dashboard-nav-item ${
+            activeTab === "upgrades" ? "active" : ""
+          }`}
+          onClick={() => handleTabClick("upgrades")}
+        >
+          Upgrades
+        </li>
+        <li
+          className={`dashboard-nav-item ${
+            activeTab === "tab3" ? "active" : ""
+          }`}
+          onClick={() => handleTabClick("tab3")}
+        >
+          tab 3
+        </li>
+      </ul>
+      <div className="top-divider"></div>
+      <div className="dashboard-body">
+        {activeTab === "editor" && <div>Content for Tab 1</div>}
+        {activeTab === "upgrades" && <div>Content for Tab 2</div>}
+        {activeTab === "tab3" && <div>Content for Tab 3</div>}
+      </div>
+    </div>
+  );
+};
+export default Dashboard;
---
+++ server/schemas/resolvers.js
@@ -0,0 +1,43 @@
+const { AuthenticationError } = require("apollo-server-express");
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    users: async () => {
+      return User.find();
+    },
+    user: async (parent, { username }) => {
+      return User.findOne({ username });
+    },
+  },
+
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError(
+          "User was not found with this email... Please try again!"
+        );
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Please check the credentials!");
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+  },
+};
+
+module.exports = resolvers;
