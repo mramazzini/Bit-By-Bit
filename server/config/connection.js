1d8a0828febb2a2dcd18dded619ac00a7a0889c5
mramazzini <mramazzini123@gmail.com>
2023-03-16 22:40:13 -0500

    prepared for heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/bit-by-bit",
-  {
+mongoose
+  .connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/bit-by-bit", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  }
-);
+  })
+  .then(() => console.log("MongoDB has been connected"))
+  .catch((err) => console.log(err));
 
 module.exports = mongoose.connection;
